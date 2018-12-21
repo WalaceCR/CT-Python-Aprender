5e1148156926757c6eea2294869c5fda136a39c6
WalaceCR <walace.dacunha@yahoo.com.br>
2018-12-21 17:37:57 -0200

    Melhorias de sintaxe adicionando mais arquivos para referências.

---
+++ src/files_old/Estudo sintaxe Alura.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+Este é um arquivo de script temporário.
+"""
+"ajuda sobre a função print"
+help(print)
+
+
+print("eu", "tu", "ele", "nós", "vós")
+
+
+estrada = 666
+nome = "Walace"
+
+print(type(estrada))
+print(type(nome))
+
+dia = 14
+mes = 12
+ano = 2018
+
+print(dia, mes, ano, sep="/")
---
+++ src/files_old/Estudo sintaxe.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+Este é um arquivo de script temporário.
+"""
+
+nota = 5
+boleano = 1
+testearray = [1, 2, 3, 4, 'Casa', 5.9983784673746374673, boleano]
+
+if nota == 10:
+    print('Aprovado')
+else:
+    print('Reprovado')
+    
+    
+for item in testearray:
+    print(item)    
