3bec01f554452322bc1a94506bf378d8b1874f06
WalaceCR <walace.dacunha@yahoo.com.br>
2019-01-08 15:07:48 -0200

    Aprendendo Randomicos com Python Tratamento de strings

---
+++ src/main/start_randomico.py
@@ -0,0 +1,11 @@
+
+import random
+
+
+def start():
+    numero_randomico = random.random() * 100
+    numero_convertido = int(numero_randomico)
+    print(numero_convertido)
+
+
+start()
---
+++ src/main/start_tratamento_string.py
@@ -0,0 +1,10 @@
+
+
+def init():
+    print("R$ {}".format(45.90))
+    print("R$ {:.2f}".format(45.90))
+    print("R$ {:08.2f}".format(45.90))
+    print("Data {:2d}/{:2d}/{:4d}")
+
+
+init()
